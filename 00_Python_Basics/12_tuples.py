0b933e475a9f392e1347783f727942e5482c2ab1
Yash Prajapati <yashpraja9120@gmail.com>
2026-08-19 20:11:38 +0530

    Add tuple operations

--- 00_Python_Basics/12_tuples.py
+++ 00_Python_Basics/12_tuples.py
@@ -1,8 +1,21 @@
 #Create 1st tuple with values -> (10, 20, 30), 2nd tuple with values -> (40,
 #50, 60):
 
-tuple1 =(10,20,30)
-tuple2 = (40,50,60)
+tuple1 = (10, 20, 30)
+tuple2 = (40, 50, 60)
 
-print(tuple1)
-print(tuple2)
+# a. Concatenate the two tuples
+t_combine = tuple1 + tuple2
+print("a.", t_combine)
+
+# b. Repeat the elements 3 times
+print("b.", t_combine * 3)
+
+# c. Access the 3rd element
+print("c.", t_combine[2])
+
+# d. Access the first three elements
+print("d.", t_combine[:3])
+
+# e. Access the last three elements
+print("e.", t_combine[-3:])
