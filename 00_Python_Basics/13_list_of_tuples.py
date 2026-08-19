b9bc257b4427e58ff0e570dd5f8a1ab71f3daca4
Yash Prajapati <yashpraja9120@gmail.com>
2026-08-19 20:17:08 +0530

    Add list of tuples

---
+++ 00_Python_Basics/13_list_of_tuples.py
@@ -0,0 +1,9 @@
+#Create a list ‘my_list’ with these elements: a. First element is a tuple with values 1, 2, 3 b. Second element is a tuple with values “a”, “b”, “c” c. Third element is a tuple with values True, False
+
+my_list = [
+    (1,2,3),
+    ("a", "b", "c")
+    (True, False)
+]
+
+print(my_list)
